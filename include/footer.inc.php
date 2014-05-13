e60e4bffb9f57c7f023ae121abc9e85a82fe0c52
Kristo <kristo@mind.ee>
2014-05-14 01:14:23 +0300

    first commit

---
+++ include/footer.inc.php
@@ -0,0 +1,3 @@
+<?php 
+echo "<p>" . date("DMY") . "</p>";
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>tester</title>
+  <meta name="description" content="tester">
+  <meta name="author" content="tester">
+
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+<p>tester</p>
+<?php include 'footer.inc.php';?>
+</body>
+</html>
